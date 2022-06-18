af00d6449429d9791fe8666190bdde76810e5443
Sirine1706 <103147939+Sirine1706@users.noreply.github.com>
2022-06-18 21:51:53 +0200

    task 2

---
+++ models/type.models.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const TypeSchema = new mongoose.Schema({
+	fruit: { type: Boolean },
+	vegetable: { type: Boolean },
+},
+{timestamps: true}
+);
+
+module.exports = mongoose.model("type", TypeSchema);
